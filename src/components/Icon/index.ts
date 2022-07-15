3683980af2c726116e093aa14ad6ad2ee540d0ae
Alexandre Aragão <alexr.aragao@gmail.com>
2022-07-15 14:28:26 -0300

    feat(icons): add base icons

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,10 @@
+export { default as SvgArrowBack } from "./ArrowBack";
+export { default as SvgClose } from "./Close";
+export { default as SvgContentCopy } from "./ContentCopy";
+export { default as SvgHistory } from "./History";
+export { default as SvgHome } from "./Home";
+export { default as SvgMapMarker } from "./MapMarker";
+export { default as SvgProfile } from "./Profile";
+export { default as SvgSettings } from "./Settings";
+export { default as SvgStarFilled } from "./StarFilled";
+export { default as SvgStarOutline } from "./StarOutline";
